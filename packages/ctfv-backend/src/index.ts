d8300e1d2456a171c36d16a8ef8af25ece69c7b3
Sagnik Mandal <acriticalcynic@outlook.com>
2024-09-20 12:56:06 +0530

    Zod-Openapi Trial

--- packages/ctfv-backend/src/index.ts
+++ packages/ctfv-backend/src/index.ts
@@ -3,6 +3,8 @@ import { Context, Hono } from "hono";
 import { cors } from "hono/cors";
 import { JwtVariables } from "hono/jwt";
 import { logger } from "hono/logger";
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono, createRoute, z } from '@hono/zod-openapi'
 
 import { Bindings } from "../env";
 import * as schema from "./db/schema";
@@ -11,16 +13,28 @@ import userRouter from "./routes/user";
 
 type Variables = JwtVariables;
 
-const app = new Hono<{ Bindings: Bindings; Variables: Variables }>();
+const app = new OpenAPIHono<{ Bindings: Bindings; Variables: Variables }>();
 
 app.use(
   "*",
   cors({
     origin: (origin) => origin,
     credentials: true,
-  }),
+  })
 );
+
 app.use(logger());
+
+app.get('/ui', swaggerUI({ url: '/doc' }));
+
+app.doc('/doc', {
+  info: {
+    title: 'My API',
+    version: 'v1',
+  },
+  openapi: '3.1.0',
+});
+app.get('/ui', swaggerUI({ url: '/doc' }))
 
 app.route("api/users", userRouter);
 app.route("api/challenges", challengesRouter);
--- packages/ctfv-backend/src/routes/user.ts
+++ packages/ctfv-backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { JwtVariables, sign } from "hono/jwt";
+import { OpenAPIHono, createRoute, z } from '@hono/zod-openapi'
 
 import { getDB } from "..";
 import { Bindings } from "../../env";
@@ -10,158 +11,240 @@ import { authMiddleware } from "../middlewares/auth";
 
 type Variables = JwtVariables;
 
-const userRouter = new Hono<{ Bindings: Bindings; Variables: Variables }>();
+const userRouter = new OpenAPIHono<{ Bindings: Bindings; Variables: Variables }>();
 
-userRouter.post("/auth/register", async (c) => {
-  try {
-    const db = getDB(c);
-    const { email, password, username, isAdmin } = await c.req.json();
-
-    // TODO: Remove isAdmin from the registration form
-    if (typeof isAdmin !== "boolean") {
-      return c.json({ error: "isAdmin must be boolean" }, 400);
+userRouter.openapi(
+  createRoute({
+    method: 'post',
+    path: '/auth/register',
+    request: {
+      body: {
+        content: {
+          'application/json': {
+            schema: z.object({
+              email: z.string().email(),
+              password: z.string(),
+              username: z.string(),
+              isAdmin: z.boolean().optional(),
+            }),
+          },
+        },
+      }
+    },
+    responses: {
+      200: {
+        description: 'User registration success',
+        content: {
+          'application/json': {
+            schema: z.object({
+              message: z.string(),
+              token: z.string(),
+              user: z.object({
+                id: z.string(),
+                email: z.string(),
+                username: z.string(),
+                isAdmin: z.boolean(),
+              }),
+            }),
+          },
+        },
+      },
+      400: {
+        description: 'Bad request',
+        content: {
+          'application/json': {
+            schema: z.object({
+              message: z.string(),
+            }),
+          },
+        },
+      },
+      500: {
+        description: 'Internal server error',
+        content: {
+          'application/json': {
+            schema: z.object({
+              message: z.string(),
+            }),
+          },
+        },
+      },
+    },
+  }),
+  async (c) => {
+    try {
+      const db = getDB(c);
+      const { email, password, username, isAdmin } = await c.req.json();
+  
+      if (typeof isAdmin !== "boolean") {
+        return c.json({ message: "ERROR: isAdmin must be boolean" }, 400);
+      }
+  
+      const existingUser = await db.query.users.findFirst({
+        where: eq(schema.users.email, email),
+      });
+  
+      if (existingUser) {
+        return c.json({ message: "ERROR: User already exists" }, 400);
+      }
+  
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const newUser = await db
+        .insert(schema.users)
+        .values({
+          email,
+          password: hashedPassword,
+          username,
+          isAdmin,
+        })
+        .returning()
+        .get();
+  
+      if (!c.env.AUTH_SECRET) {
+        console.error("AUTH_SECRET is not defined");
+        return c.json({ message: "ERROR: Internal server error" }, 500);
+      }
+  
+      const token = await sign({ userId: newUser.id }, c.env.AUTH_SECRET);
+  
+      return c.json({
+        message: "User registered successfully",
+        token,
+        user: {
+          id: newUser.id,
+          email: newUser.email,
+          username: newUser.username,
+          isAdmin: newUser.isAdmin,
+        },
+      });
+    } catch (error) {
+      console.error("Error in /api/auth/register:", error);
+      return c.json({ message: "ERROR: Internal server error" }, 500);
     }
+  }
+);
 
-    const existingUser = await db.query.users.findFirst({
+// OpenAPI for user login
+userRouter.openapi(
+  createRoute({
+    method: 'post',
+    path: '/auth/login',
+    request: {
+      body: {
+        content: {
+          'application/json': {
+            schema: z.object({
+              email: z.string().email(),
+              password: z.string(),
+            }),
+          },
+        },
+      }
+    },
+    responses: {
+      200: {
+        description: 'User login success',
+        content: {
+          'application/json': {
+            schema: z.object({
+              message: z.string(),
+              token: z.string(),
+              user: z.object({
+                id: z.string(),
+                email: z.string(),
+                username: z.string(),
+                isAdmin: z.boolean(),
+              }),
+            }),
+          },
+        },
+      },
+      401: {
+        description: 'Unauthorized',
+        content: {
+          'application/json': {
+            schema: z.object({
+              message: z.string(),
+            }),
+          },
+        },
+      },
+    },
+  }),
+  async (c) => {
+    const db = getDB(c);
+    const { email, password } = await c.req.json();
+  
+    const user = await db.query.users.findFirst({
       where: eq(schema.users.email, email),
     });
-
-    if (existingUser) {
-      return c.json({ error: "User already exists" }, 400);
+  
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return c.json({ message: "ERROR: Invalid credentials" }, 401);
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await db
-      .insert(schema.users)
-      .values({
-        email,
-        password: hashedPassword,
-        username,
-        isAdmin,
-      })
-      .returning()
-      .get();
-
-    // Check if AUTH_SECRET is defined
-    if (!c.env.AUTH_SECRET) {
-      console.error("AUTH_SECRET is not defined in the environment");
-      return c.json({ error: "Internal server error" }, 500);
-    }
-
-    const token = await sign({ userId: newUser.id }, c.env.AUTH_SECRET);
-
+  
+    const token = await sign({ userId: user.id }, c.env.AUTH_SECRET);
+  
     return c.json({
+      message: "User logged in successfully",
       token,
       user: {
-        id: newUser.id,
-        email: newUser.email,
-        username: newUser.username,
-        isAdmin: newUser.isAdmin,
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        isAdmin: user.isAdmin,
       },
     });
-  } catch (error) {
-    console.error("Error in /api/auth/register:", error);
-    return c.json({ error: "Internal server error" }, 500);
   }
-});
+);
 
-userRouter.post("/auth/login", async (c) => {
-  const db = getDB(c);
-  const { email, password } = await c.req.json();
-
-  const user = await db.query.users.findFirst({
-    where: eq(schema.users.email, email),
-  });
-
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    return c.json({ error: "Invalid credentials" }, 401);
-  }
-
-  const token = await sign({ userId: user.id }, c.env.AUTH_SECRET);
-
-  return c.json({
-    token,
-    user: {
-      id: user.id,
-      email: user.email,
-      username: user.username,
-      isAdmin: user.isAdmin,
+// OpenAPI for fetching all users
+userRouter.openapi(
+  createRoute({
+    method: 'get',
+    path: '/',
+    responses: {
+      200: {
+        description: 'Get all users',
+        content: {
+          'application/json': {
+            schema: z.array(
+              z.object({
+                id: z.string(),
+                username: z.string(),
+                email: z.string(),
+                rollNo: z.string().nullable(),
+                instituteName: z.string().nullable(),
+                website: z.string().nullable(),
+                affiliation: z.string().nullable(),
+                country: z.string().nullable(),
+                isAdmin: z.boolean(),
+              })
+            ),
+          },
+        },
+      },
     },
-  });
-});
-
-userRouter.get("/", async (c) => {
-  const db = getDB(c);
-  const users = await db.query.users.findMany({
-    columns: {
-      id: true,
-      username: true,
-      email: true,
-      rollNo: true,
-      instituteName: true,
-      website: true,
-      affiliation: true,
-      country: true,
-      isAdmin: true,
-    },
-  });
-  return c.json(users);
-});
-
-// Get user profile
-userRouter.get("/:id", authMiddleware, async (c) => {
-  const db = getDB(c);
-  const userId = c.req.param("id");
-  const user = await db.query.users.findFirst({
-    where: eq(schema.users.id, userId),
-    columns: {
-      id: true,
-      username: true,
-      email: true,
-      rollNo: true,
-      instituteName: true,
-      website: true,
-      affiliation: true,
-      country: true,
-      isAdmin: true,
-    },
-  });
-
-  if (!user) {
-    return c.json({ error: "User not found" }, 404);
-  }
-
-  return c.json(user);
-});
-
-// Update user profile
-userRouter.put("/:id", authMiddleware, async (c) => {
-  const db = getDB(c);
-  const userId = c.req.param("id");
-  const { rollNo, instituteName, website, affiliation, country } =
-    await c.req.json();
-
-  if (userId !== c.get("jwtPayload").userId) {
-    return c.json({ error: "Unauthorized" }, 403);
-  }
-
-  const updatedUser = await db
-    .update(schema.users)
-    .set({ rollNo, instituteName, website, affiliation, country })
-    .where(eq(schema.users.id, userId))
-    .returning({
-      id: schema.users.id,
-      username: schema.users.username,
-      email: schema.users.email,
-      rollNo: schema.users.rollNo,
-      instituteName: schema.users.instituteName,
-      website: schema.users.website,
-      affiliation: schema.users.affiliation,
-      country: schema.users.country,
+  }),
+  async (c) => {
+    const db = getDB(c);
+    const users = await db.query.users.findMany({
+      columns: {
+        id: true,
+        username: true,
+        email: true,
+        rollNo: true,
+        instituteName: true,
+        website: true,
+        affiliation: true,
+        country: true,
+        isAdmin: true,
+      },
     });
 
-  return c.json(updatedUser[0]);
-});
+    return c.json(users);
+  }
+);
 
 export default userRouter;
